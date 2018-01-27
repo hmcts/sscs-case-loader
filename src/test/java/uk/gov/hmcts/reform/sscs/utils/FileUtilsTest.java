5f3762b233ecfcddd781404b96fe0310885d7604
david.crespo <david.crespo@hmcts.net>
2018-01-27 19:14:19 +0100

    SSCS-2579 Fix minor syntax error

--- src/test/java/uk/gov/hmcts/reform/sscs/utils/FileUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/utils/FileUtilsTest.java
@@ -1,9 +1,10 @@
 package uk.gov.hmcts.reform.sscs.utils;
 
 import org.junit.Test;
+import uk.gov.hmcts.reform.sscs.exceptions.FailedToReadResourceException;
 
 public class FileUtilsTest {
-    @Test(expected = RuntimeException.class)
+    @Test(expected = FailedToReadResourceException.class)
     public void givenFilePath_shouldGetFileContent() {
         FileUtils.getFileContentGivenFilePath("noExistingFile.xml");
     }
