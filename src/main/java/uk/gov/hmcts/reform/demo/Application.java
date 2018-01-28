b1b639cbe1c05c9c3734afc3451ff989b38478a4
david.crespo <david.crespo@hmcts.net>
2018-01-28 21:56:57 +0100

    Truncate history

---
+++ src/main/java/uk/gov/hmcts/reform/demo/Application.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.reform.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+// TODO: change the name of this class to one matching your project, e.g. DraftStoreApplication
+@SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
