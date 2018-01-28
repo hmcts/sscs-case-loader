a44d88fd26d51c550c0393cf8efd194ef89648bb
david.crespo <david.crespo@hmcts.net>
2018-01-28 01:20:45 +0100

    SSCS-2579 Code refactoring
    Annotate model classes with Lombok to get rid of boiler plate code

--- src/test/java/uk/gov/hmcts/reform/sscs/AppealUtils.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/AppealUtils.java
@@ -10,6 +10,10 @@ public final class AppealUtils {
 
     public static Appeal buildAppeal() {
         Appellant appellant = new Appellant(new Name("Mr", "User", "Test"));
-        return new Appeal("2017-10-08", "It was missing", appellant);
+        return Appeal.builder()
+            .mrnDate("2017-10-08")
+            .mrnMissingReason("It was missing")
+            .appellant(appellant)
+            .build();
     }
 }
--- src/test/java/uk/gov/hmcts/reform/sscs/services/SaveSimpleCaseIntoCcd.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/services/SaveSimpleCaseIntoCcd.java
@@ -1,12 +1,15 @@
 package uk.gov.hmcts.reform.sscs.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.sscs.AppealUtils;
+import uk.gov.hmcts.reform.sscs.models.CaseData;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -24,4 +27,17 @@ public class SaveSimpleCaseIntoCcd {
         coreCaseDataService.startEventAndSaveGivenCase(AppealUtils.buildAppeal());
     }
 
+    @Test
+    @Ignore
+    public void givenCaseData_shouldBeTransformed() throws Exception {
+        CaseData caseData = CaseData.builder()
+            .appeal(AppealUtils.buildAppeal())
+            .build();
+
+        ObjectMapper mapper = Jackson2ObjectMapperBuilder.json()
+            .indentOutput(true)
+            .build();
+
+        System.out.println(mapper.writeValueAsString(caseData));
+    }
 }
