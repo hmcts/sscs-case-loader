05a116252aecee5cff4c6ce794a3255bc066be94
david.crespo <david.crespo@hmcts.net>
2018-01-28 01:54:06 +0100

    SSCS-2579 Code refactoring
    Annotate rest of model classes with Lombok
    to get rid of boiler plate code.

--- src/main/java/uk/gov/hmcts/reform/sscs/config/properties/CoreCaseDataProperties.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/properties/CoreCaseDataProperties.java
@@ -3,8 +3,10 @@ package uk.gov.hmcts.reform.sscs.config.properties;
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
 @Configuration
+@Validated
 @ConfigurationProperties(prefix = "core_case_data")
 public class CoreCaseDataProperties {
     @NotBlank
--- src/main/java/uk/gov/hmcts/reform/sscs/models/Appellant.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/models/Appellant.java
@@ -1,13 +1,10 @@
 package uk.gov.hmcts.reform.sscs.models;
 
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
 public class Appellant {
     private Name name;
-
-    public Appellant(Name name) {
-        this.name = name;
-    }
-
-    public Name getName() {
-        return name;
-    }
 }
