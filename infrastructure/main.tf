3985f552cf829e0c22d6650df9cea74d7b44540d
Sathish Jeganathan <Satish.Jaganathan@HMCTS.NET>
2018-01-29 10:37:54 +0000

    SSCS-2557 Added the latest infrastructure code

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,12 +1,11 @@
 module "sscs-case-loader" {
-  source   = "git@github.com:contino/moj-module-webapp?ref=0.0.78"
+  source   = "git@github.com:contino/moj-module-webapp?ref=master"
   product  = "${var.product}-case-loader"
   location = "${var.location}"
   env      = "${var.env}"
-  asename  = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  ilbIp    = "${var.ilbIp}"
 
   app_settings = {
-    SSCS_CASE_LOADER_PORT = "${var.sscs_case_loader_server_port}"
     MANAGEMENT_SECURITY_ENABLED = "${var.management_security_enabled}"
   }
 }
