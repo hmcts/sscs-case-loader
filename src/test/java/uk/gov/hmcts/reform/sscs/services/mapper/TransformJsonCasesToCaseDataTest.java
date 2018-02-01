e297bd5406a7adc02ab18c83fee9cbdf9c23869b
david.crespo <david.crespo@hmcts.net>
2018-02-01 23:53:39 +0100

    SSCS-2638 Minor Refactor
    Clean up the jode-time dependency.
    Parametrise testig methods to avoid duplication.

--- src/test/java/uk/gov/hmcts/reform/sscs/services/mapper/TransformJsonCasesToCaseDataTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/services/mapper/TransformJsonCasesToCaseDataTest.java
@@ -5,35 +5,52 @@ import static net.javacrumbs.jsonunit.JsonAssert.assertJsonEquals;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
 import uk.gov.hmcts.reform.sscs.models.serialize.ccd.CaseData;
 import uk.gov.hmcts.reform.sscs.utils.FileUtils;
 
 
-@RunWith(SpringRunner.class)
+@RunWith(JUnitParamsRunner.class)
 @SpringBootTest
 public class TransformJsonCasesToCaseDataTest {
 
     @Autowired
     private TransformJsonCasesToCaseData transformJsonCasesToCaseData;
 
+    @ClassRule
+    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
+
+    @Rule
+    public final SpringMethodRule springMethodRule = new SpringMethodRule();
+
     @Test
-    public void givenJsonCases_shouldBeMappedIntoCaseData() throws IOException {
+    @Parameters({
+        "src/test/resources/SSCS_Extract_Delta_2017-05-24-16-14-19.json, "
+            + "src/test/resources/CaseData.json",
+        "src/test/resources/SSCS_Extract_Delta_2017-05-24-16-14-19_With_Dob.json, "
+            + "src/test/resources/CaseDataWithDob.json"
+    })
+    public void givenJsonCases_shouldBeMappedIntoCaseData(String jsonCasesPath, String expectedCaseDataPath)
+        throws IOException {
         // Given
-        String jsonCases = FileUtils
-                .getFileContentGivenFilePath("src/test/resources/SSCS_Extract_Delta_2017-05-24-16-14-19.json");
+        String jsonCases = FileUtils.getFileContentGivenFilePath(jsonCasesPath);
 
         // When
         CaseData caseData = transformJsonCasesToCaseData.transform(jsonCases);
 
         // Should
         String actualCaseDataString = transformCaseDataToCaseDataString(caseData);
-        String expectedCaseDataString = FileUtils.getFileContentGivenFilePath("src/test/resources/CaseData.json");
+        String expectedCaseDataString = FileUtils.getFileContentGivenFilePath(expectedCaseDataPath);
         assertJsonEquals(expectedCaseDataString, actualCaseDataString);
     }
 
@@ -42,21 +59,5 @@ public class TransformJsonCasesToCaseDataTest {
             .indentOutput(true)
             .build();
         return mapper.writeValueAsString(caseData);
-    }
-
-    @Test
-    public void givenJsonCases_shouldBeMappedIntoCaseDatawithDob() throws IOException {
-        // Given
-        String jsonCases = FileUtils
-                .getFileContentGivenFilePath("src/test/resources/SSCS_Extract_Delta_2017-05-24-16-14-19_With_Dob.json");
-
-        // When
-        CaseData caseData = transformJsonCasesToCaseData.transform(jsonCases);
-
-        // Should
-        String actualCaseDataString = transformCaseDataToCaseDataString(caseData);
-        String expectedCaseDataString = FileUtils
-                .getFileContentGivenFilePath("src/test/resources/CaseDataWithDob.json");
-        assertJsonEquals(expectedCaseDataString, actualCaseDataString);
     }
 }
