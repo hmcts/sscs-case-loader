8728bab677e6342d12acc430da6bf31e1247a0b4
Santhosh Avirneni <savirneni@Santhoshs-MacBook-Pro.local>
2018-02-02 15:16:47 +0000

    SSCS-2648[SA]:Wire up all components for ccd loader app

--- src/e2e/java/uk.gov.hmcts.reform.sscs/ProcessFileAndSaveIntoCcd.java
+++ src/e2e/java/uk.gov.hmcts.reform.sscs/ProcessFileAndSaveIntoCcd.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import uk.gov.hmcts.reform.sscs.services.CaseLoaderService;
+import uk.gov.hmcts.reform.sscs.services.service.CaseLoaderService;
 
 /**
  * Class for testing purpose.
--- src/main/java/uk/gov/hmcts/reform/sscs/CaseLoaderApp.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/CaseLoaderApp.java
@@ -1,12 +1,17 @@
 package uk.gov.hmcts.reform.sscs;
 
+import com.jcraft.jsch.JSch;
+import java.io.IOException;
+import javax.xml.stream.XMLStreamException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import uk.gov.hmcts.reform.sscs.services.CaseLoaderService;
+import org.springframework.context.annotation.Bean;
+import org.xml.sax.SAXException;
+import uk.gov.hmcts.reform.sscs.services.service.CaseLoaderService;
 
 @SpringBootApplication
 @EnableCircuitBreaker
@@ -17,17 +22,18 @@ public class CaseLoaderApp implements CommandLineRunner {
     @Autowired
     private CaseLoaderService caseLoaderService;
 
+    @Bean
+    public JSch jschSshChannel() {
+        return new JSch();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(CaseLoaderApp.class, args);
     }
 
     @Override
-    public void run(String... args) {
-        try {
-            caseLoaderService.process();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void run(String... args) throws IOException, SAXException, XMLStreamException {
+        caseLoaderService.process();
     }
 
 
