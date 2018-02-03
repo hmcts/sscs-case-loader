39838444612759d37daed847092fe39464f57d7c
david.crespo <david.crespo@hmcts.net>
2018-02-03 02:04:04 +0100

    SSCS-2648 Rebase master and comment out code beause it fails to load App context

--- src/main/java/uk/gov/hmcts/reform/sscs/services/service/CaseLoaderService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/services/service/CaseLoaderService.java
@@ -5,19 +5,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import javax.xml.stream.XMLStreamException;
-
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.xml.sax.SAXException;
-
-import uk.gov.hmcts.reform.sscs.models.JsonFiles;
 import uk.gov.hmcts.reform.sscs.models.XmlFiles;
 import uk.gov.hmcts.reform.sscs.services.ccd.CoreCaseDataService;
 import uk.gov.hmcts.reform.sscs.services.mapper.TransformJsonCasesToCaseData;
@@ -42,23 +35,24 @@ public class CaseLoaderService {
 
 
     public void process() throws IOException, SAXException, XMLStreamException {
-        List<InputStream> inputStreamList = sftpSshService.readExtractFiles();
-        Optional<XmlFiles> optionalXmlFiles;
-        try {
-            optionalXmlFiles = Optional.of(XmlFiles.builder().delta(convert(inputStreamList.get(0),
-                StandardCharsets.UTF_8)).build());
-        } catch (IndexOutOfBoundsException ie) {
-            optionalXmlFiles = Optional.empty();
-            log.error(ie.getMessage());
-        }
-
-        if (optionalXmlFiles.isPresent()) {
-            XmlFiles xmlFiles = optionalXmlFiles.get();
-            validateXmls(xmlFiles);
-            JsonFiles jsonFiles = transformXmlFilesToJsonFiles.transform(xmlFiles);
-            coreCaseDataService.startEventAndSaveGivenCase(transformJsonCasesToCaseData.transform(jsonFiles
-                .getDelta().toString()));
-        }
+        //TODO application context fails if we uncomment this. Fix it.
+    //        List<InputStream> inputStreamList = sftpSshService.readExtractFiles();
+    //        Optional<XmlFiles> optionalXmlFiles;
+    //        try {
+    //            optionalXmlFiles = Optional.of(XmlFiles.builder().delta(convert(inputStreamList.get(0),
+    //                StandardCharsets.UTF_8)).build());
+    //        } catch (IndexOutOfBoundsException ie) {
+    //            optionalXmlFiles = Optional.empty();
+    //            log.error(ie.getMessage());
+    //        }
+    //
+    //        if (optionalXmlFiles.isPresent()) {
+    //            XmlFiles xmlFiles = optionalXmlFiles.get();
+    //            validateXmls(xmlFiles);
+    //            JsonFiles jsonFiles = transformXmlFilesToJsonFiles.transform(xmlFiles);
+    //            coreCaseDataService.startEventAndSaveGivenCase(transformJsonCasesToCaseData.transform(jsonFiles
+    //                .getDelta().toString()));
+    //        }
     }
 
     private String convert(InputStream inputStream, Charset charset) throws IOException {
