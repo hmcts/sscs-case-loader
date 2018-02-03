0c502200c0809e529d451a73a164639da6b678c8
david.crespo <david.crespo@hmcts.net>
2018-02-03 23:59:54 +0100

    SSCS-2648
    Upgrade spring-boot version.
    Add commons-io dependency for files and stream handling.
    XmlFiles store delta and ref content as string.
    Wire up rest of services in the process method.
    Adjust test code because of changes mentioned above.

--- src/main/java/uk/gov/hmcts/reform/sscs/services/CaseLoaderService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/services/CaseLoaderService.java
@@ -2,13 +2,19 @@ package uk.gov.hmcts.reform.sscs.services;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import javax.xml.stream.XMLStreamException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.xml.sax.SAXException;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.sscs.exceptions.GapsValidationException;
+import uk.gov.hmcts.reform.sscs.models.JsonFiles;
+import uk.gov.hmcts.reform.sscs.models.XmlFiles;
+import uk.gov.hmcts.reform.sscs.models.serialize.ccd.CaseData;
 import uk.gov.hmcts.reform.sscs.services.ccd.CoreCaseDataService;
 import uk.gov.hmcts.reform.sscs.services.mapper.TransformJsonCasesToCaseData;
 import uk.gov.hmcts.reform.sscs.services.mapper.TransformXmlFilesToJsonFiles;
@@ -39,19 +45,36 @@ public class CaseLoaderService {
 
     public void process() {
         List<InputStream> inputStreamList = sftpSshService.readExtractFiles();
-        log.info("*** case-loader *** xml files read from SFTP successfully");
+        log.info("*** case-loader *** Read xml files from SFTP successfully");
         inputStreamList.forEach(inputStream -> {
+            CaseData caseData = null;
             try {
                 xmlValidator.validateXml(inputStream, "Ref");
-                log.info("*** case-loader *** xml files validated successfully");
-            } catch (IOException | SAXException | XMLStreamException e) {
+                log.info("*** case-loader *** Validate xml files successfully");
+            } catch (SAXException | XMLStreamException | IOException e) {
                 throw new GapsValidationException("Failed to validate xml", e);
             }
-            // TODO: 03/02/2018 change services to take inputStream as argument
-            //XmlFiles.builder().delta(inputStream);
-            //JsonFiles jsonFiles = transformXmlFilesToJsonFiles.transform(xmlFiles);
-            //coreCaseDataService.startEventAndSaveGivenCase(transformJsonCasesToCaseData.transform(jsonFiles
-            //.getDelta().toString()));
+            try {
+                caseData = transformFromXmlFilesToCaseData(inputStream);
+                log.info("*** case-loader *** Transform xml files into CCD Cases successfully");
+            } catch (IOException e) {
+                // TODO: 03/02/2018 create custom exception here
+                throw new RuntimeException("Failed to transform xml to CCD data", e);
+            }
+            CaseDetails caseDetails = coreCaseDataService.startEventAndSaveGivenCase(caseData);
+            log.info("*** case-loader *** Save Case into CCD  successfully: %s", caseDetails);
         });
     }
+
+    private CaseData transformFromXmlFilesToCaseData(InputStream inputStream) throws IOException {
+        JsonFiles jsonCases = transformXmlFilesToJsonFiles.transform(
+            buildXmlFilesFromInputStream(inputStream));
+        return transformJsonCasesToCaseData.transform(jsonCases.getDelta().toString());
+    }
+
+    private XmlFiles buildXmlFilesFromInputStream(InputStream refAsStream) throws IOException {
+        String refAsString = IOUtils.toString(refAsStream, StandardCharsets.UTF_8.name());
+        return XmlFiles.builder().ref(refAsString).build();
+    }
+
 }
