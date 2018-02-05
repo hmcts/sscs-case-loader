83d75b6cce89083271ad35bb3442e8ed69fab01d
david.crespo <david.crespo@hmcts.net>
2018-02-05 12:41:33 +0100

    SSCS-2648 Bring branch up-to-date with master
    Rebase with master and fix conflicts.

--- src/e2e/java/uk.gov.hmcts.reform.sscs/ProcessFileAndSaveIntoCcd.java
+++ src/e2e/java/uk.gov.hmcts.reform.sscs/ProcessFileAndSaveIntoCcd.java
@@ -1,14 +1,11 @@
 package uk.gov.hmcts.reform.sscs;
 
-import static org.junit.Assert.assertNotNull;
-
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import uk.gov.hmcts.reform.sscs.services.CaseLoaderService;
 
 /**
@@ -25,8 +22,7 @@ public class ProcessFileAndSaveIntoCcd {
 
     @Test
     @Ignore
-    public void givenACase_shouldBeSaveIntoCcd() throws Exception {
-        assertNotNull(caseLoaderService);
+    public void givenACase_shouldBeSaveIntoCcd() {
         caseLoaderService.process();
     }
 
