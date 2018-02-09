c52719404a645aa36dbf57847f511a82b5b5dfc3
david.crespo <david.crespo@hmcts.net>
2018-02-09 12:44:59 +0100

    SSCS-2679 Improve integration test as suggested from code review

--- src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/services/CaseLoaderServiceTest.java
+++ src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/services/CaseLoaderServiceTest.java
@@ -2,14 +2,17 @@ package uk.gov.hmcts.reform.sscs.services;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Vector;
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,21 +23,21 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
 import uk.gov.hmcts.reform.sscs.TestCaseLoaderApp;
-import uk.gov.hmcts.reform.sscs.models.GapsInputStream;
 import uk.gov.hmcts.reform.sscs.models.idam.Authorize;
 import uk.gov.hmcts.reform.sscs.models.serialize.ccd.CaseData;
 import uk.gov.hmcts.reform.sscs.services.ccd.CoreCaseDataService;
 import uk.gov.hmcts.reform.sscs.services.idam.IdamApiClient;
-import uk.gov.hmcts.reform.sscs.services.sftp.SftpSshService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TestCaseLoaderApp.class)
 public class CaseLoaderServiceTest {
 
     @MockBean
-    private SftpSshService sftpSshService;
+    private JSch jschSshChannel;
     @MockBean
     private CoreCaseDataApi coreCaseDataApi;
     @MockBean
@@ -50,13 +53,25 @@ public class CaseLoaderServiceTest {
     private static final String DELTA_XML = "src/test/resources/SSCS_Extract_Delta_2017-05-24-16-14-19.xml";
 
     @Test
-    public void givenDeltaXmlInSftp_shouldBeSavedIntoCcd() throws IOException {
-        List<GapsInputStream> inputStreamList = new ArrayList<>(1);
-        InputStream stream = FileUtils.openInputStream(new File(DELTA_XML));
-        inputStreamList.add(GapsInputStream.builder().inputStream(stream).isDelta(true).isReference(false)
-            .build());
+    public void givenDeltaXmlInSftp_shouldBeSavedIntoCcd() throws Exception {
+        Session session = mock(Session.class);
+        given(jschSshChannel.getSession(
+            anyString(),
+            anyString(),
+            anyInt())
+        ).willReturn(session);
 
-        given(sftpSshService.readExtractFiles()).willReturn(inputStreamList);
+        ChannelSftp channelSftp = mock(ChannelSftp.class);
+        given(session.openChannel(anyString())).willReturn(channelSftp);
+        given(channelSftp.get(anyString())).willReturn(FileUtils.openInputStream(new File(DELTA_XML)));
+
+        ChannelSftp.LsEntry file = mock(ChannelSftp.LsEntry.class);
+        given(file.getFilename()).willReturn("SSCS_Extract_Delta");
+
+        Vector<ChannelSftp.LsEntry> fileList = new Vector<>();
+        fileList.add(file);
+
+        given(channelSftp.ls(anyString())).willReturn(fileList);
 
         given(authTokenGenerator.generate()).willReturn("s2s token");
 
@@ -68,6 +83,16 @@ public class CaseLoaderServiceTest {
             anyString(),
             anyString())
         ).willReturn(StartEventResponse.builder().build());
+
+        given(coreCaseDataApi.submitForCaseworker(
+            anyString(),
+            anyString(),
+            anyString(),
+            anyString(),
+            anyString(),
+            eq(Boolean.TRUE),
+            any(CaseDataContent.class)
+        )).willReturn(CaseDetails.builder().build());
 
         given(idamApiClient.authorizeCodeType(
             anyString(),
@@ -87,6 +112,16 @@ public class CaseLoaderServiceTest {
         caseLoaderService.process();
 
         verify(coreCaseDataService).startEventAndSaveGivenCase(any(CaseData.class));
+
+        verify(coreCaseDataApi).submitForCaseworker(
+            anyString(),
+            anyString(),
+            anyString(),
+            anyString(),
+            anyString(),
+            eq(Boolean.TRUE),
+            any(CaseDataContent.class)
+        );
     }
 
 }
