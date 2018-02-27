392177018cdb96573fde191488e148828ce75dc6
Sathish Jeganathan <Satish.Jaganathan@HMCTS.NET>
2018-02-27 15:29:45 +0000

    SSCS-2274 Fixed the ordering of static in ReferenceDataFiles

--- src/test/java/uk/gov/hmcts/reform/sscs/models/ReferenceDataFiles.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/models/ReferenceDataFiles.java
@@ -2,14 +2,13 @@ package uk.gov.hmcts.reform.sscs.models;
 
 public final class ReferenceDataFiles {
 
-    private ReferenceDataFiles() {
-    }
-
     public static final String GAPS2_REFERENCE_DATA_XML =
         "reference-data/SSCS_Extract_ReferenceTest_2017-06-30-09-01-31.xml";
 
     public static final String INVALID_GAPS2_REFERENCE_DATA_XML =
         "reference-data/Invalid_SSCS_Extract_Reference.xml";
 
+    private ReferenceDataFiles() {
+    }
 
 }
