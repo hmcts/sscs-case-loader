b5e04882963b4e9b524680e2555a7c9a43fe589c
jackmaloney <jack.maloney@transformuk.com>
2018-03-01 10:11:48 +0000

    SSCS-2756 Hearing lapsed event

--- src/main/java/uk/gov/hmcts/reform/sscs/models/GapsEvent.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/models/GapsEvent.java
@@ -6,6 +6,7 @@ public enum GapsEvent {
     RESPONSE_RECEIVED("responseReceived", "Response received", "18"),
     HEARING_BOOKED("hearingBooked", "Hearing booked", "24"),
     HEARING_POSTPONED("hearingPostponed", "Hearing postponed", "27"),
+    HEARING_LAPSED("hearingLapsed", "Hearing lapsed", "56"),
     HEARING_ADJOURNED("hearingAdjourned", "Hearing adjourned", "92");
 
     private String type;
