544a528c39cff79bd21302d1218519512e869cbe
david.crespo <david.crespo@hmcts.net>
2018-03-14 17:51:54 +0000

    test improvement

--- src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/controllers/SmokeTest.java
+++ src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/controllers/SmokeTest.java
@@ -1,35 +1,34 @@
 package uk.gov.hmcts.reform.sscs.controllers;
 
-import static org.assertj.core.api.BDDAssertions.then;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class SmokeTest {
 
     @Autowired
-    private TestRestTemplate testRestTemplate;
-
-    @LocalServerPort
-    private int port;
-
+    private MockMvc mockMvc;
 
     @Test
-    public void shouldReturn200WhenSendingRequestToController() {
-
-        ResponseEntity<String> entity = testRestTemplate.getForEntity("http://localhost:" + this.port
-            + "/smoke-test", String.class);
-
-        then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    public void shouldReturn200WhenSendingRequestToController() throws Exception {
+        mockMvc.perform(get("/smoke-test"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("Hi Satya, how are you today?")));
     }
 
 }
