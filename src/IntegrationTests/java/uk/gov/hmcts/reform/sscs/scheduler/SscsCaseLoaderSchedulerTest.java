9a97feea8e8ea21a83bb9ecb1ecc5f2d022576e9
David Crespo <davidcrespoarroyo@gmail.com>
2018-03-25 13:45:40 +0200

    Task/minor refactoring (#139)
    * parametrised test to reuse code
    
    * move info log to debug
    
    * Rename test
    
    * Add more case scenarios
    
    Add test scenario to confirm process runs when the host is empty.
    Add test scenario to confirm process does not run when the host is
    equal to the value in Azure.

--- src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/scheduler/SscsCaseLoaderSchedulerTest.java
+++ src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/scheduler/SscsCaseLoaderSchedulerTest.java
@@ -3,19 +3,31 @@ package uk.gov.hmcts.reform.sscs.scheduler;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
 import org.springframework.test.util.ReflectionTestUtils;
 import uk.gov.hmcts.reform.sscs.services.CaseLoaderService;
 import uk.gov.hmcts.reform.sscs.services.sftp.SftpChannelAdapter;
 
-@RunWith(SpringRunner.class)
+@RunWith(JUnitParamsRunner.class)
 @SpringBootTest
 public class SscsCaseLoaderSchedulerTest {
+
+    // Below rules are needed to use the junitParamsRunner together with SpringRunner
+    @ClassRule
+    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
+
+    @Rule
+    public final SpringMethodRule springMethodRule = new SpringMethodRule();
 
     @MockBean
     private SftpChannelAdapter channelAdapter;
@@ -27,19 +39,17 @@ public class SscsCaseLoaderSchedulerTest {
     private SscsCaseLoaderScheduler sscsCaseLoaderScheduler;
 
     @Test
-    public void givenHttpPostIsStaging_shouldNotRunTheProcess() {
+    @Parameters({
+        "sscs-case-loader-prod-staging.scm.service.core-compute-prod.internal, 0",
+        "sscs-case-loader-prod.scm.service.core-compute-prod.internal, 1",
+        "sscs-case-loader-prod-staging, 0",
+        " , 1"
+    })
+    public void givenHostname_shouldRunTheProcessOnlyIfItIsProduction(String host, int times) {
         ReflectionTestUtils.setField(sscsCaseLoaderScheduler, "httpHost",
-            "sscs-case-loader-prod-staging.scm.service.core-compute-prod.internal");
+            host);
         sscsCaseLoaderScheduler.run();
-        verify(caseLoaderService, times(0)).process();
+        verify(caseLoaderService, times(times)).process();
 
-    }
-
-    @Test
-    public void givenHttpPostIsNotStaging_shouldRunTheProcess() {
-        ReflectionTestUtils.setField(sscsCaseLoaderScheduler, "httpHost",
-            "sscs-case-loader-prod.scm.service.core-compute-prod.internal");
-        sscsCaseLoaderScheduler.run();
-        verify(caseLoaderService, times(1)).process();
     }
 }
