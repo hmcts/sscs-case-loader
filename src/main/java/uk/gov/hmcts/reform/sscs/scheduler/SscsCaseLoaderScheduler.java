2c6842e933e7697cdecdc0eee7f2a229f0a69c5e
Bart Labno <bart.labno@hmcts.net>
2018-03-26 09:40:41 +0100

    To avoid null pointer

--- src/main/java/uk/gov/hmcts/reform/sscs/scheduler/SscsCaseLoaderScheduler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/scheduler/SscsCaseLoaderScheduler.java
@@ -27,7 +27,7 @@ public class SscsCaseLoaderScheduler {
     @Scheduled(cron = "${sscs.case.loader.cron.schedule}")
     public void run() {
         log.info("*** case-loader *** process.data: {}", processData);
-        if (processData.matches("true")) {
+        if ("true".equals(processData)) {
             try {
                 log.info("SSCS Case loader scheduler started : {} ", LocalDateTime.now());
                 caseLoaderService.process();
