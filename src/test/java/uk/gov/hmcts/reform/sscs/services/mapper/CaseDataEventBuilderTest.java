7f26d4914d12cf2be849692dab24025551c29364
david.crespo <david.crespo@hmcts.net>
2018-04-27 23:20:50 +0200

    SSCS-3014 Add new junit test
    Add test to confirm that a new postponed event is created only
    if there are no postponed events with the same date.

--- src/test/java/uk/gov/hmcts/reform/sscs/services/mapper/CaseDataEventBuilderTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/services/mapper/CaseDataEventBuilderTest.java
@@ -1,13 +1,16 @@
 package uk.gov.hmcts.reform.sscs.services.mapper;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.util.List;
 import org.junit.Ignore;
 import org.junit.Test;
 import uk.gov.hmcts.reform.sscs.models.GapsEvent;
 import uk.gov.hmcts.reform.sscs.models.deserialize.gaps2.AppealCase;
+import uk.gov.hmcts.reform.sscs.models.serialize.ccd.Event;
 import uk.gov.hmcts.reform.sscs.models.serialize.ccd.Events;
 
 public class CaseDataEventBuilderTest extends CaseDataBuilderBaseTest {
@@ -20,7 +23,7 @@ public class CaseDataEventBuilderTest extends CaseDataBuilderBaseTest {
         AppealCase appealCaseWithMinorStatusId26 = AppealCase.builder()
             .appealCaseCaseCodeId("1")
             .majorStatus(super.buildMajorStatusGivenStatuses(GapsEvent.APPEAL_RECEIVED))
-            .minorStatus(super.getMinorStatusId26())
+            .minorStatus(super.getMinorStatusId26(TEST_DATE2))
             .build();
 
         events = caseDataEventBuilder.buildPostponedEvent(
@@ -37,16 +40,42 @@ public class CaseDataEventBuilderTest extends CaseDataBuilderBaseTest {
     }
 
     @Test
-    public void givenAPostponedEventIsAlreadyPresentThenAnotherPostponedWithSameDateIsNotCreated() {
-        AppealCase appealWithPostponedEventAndMinorStatus = AppealCase.builder()
+    public void givenAnExistingPostponedEventAndMinorStatusId26WithSameDatesThenNoNewPostponedEventIsCreated() {
+        AppealCase appealWithPostponedAndMinorStatusWithSameDated = AppealCase.builder()
             .appealCaseCaseCodeId("1")
             .majorStatus(super.buildMajorStatusGivenStatuses(GapsEvent.APPEAL_RECEIVED, GapsEvent.HEARING_POSTPONED))
-            .minorStatus(super.getMinorStatusId26())
+            .minorStatus(super.getMinorStatusId26(TEST_DATE2))
             .build();
 
-        events = caseDataEventBuilder.buildPostponedEvent(appealWithPostponedEventAndMinorStatus);
+        events = caseDataEventBuilder.buildPostponedEvent(appealWithPostponedAndMinorStatusWithSameDated);
 
         assertTrue("Postponed event should not be created here", events.isEmpty());
+    }
+
+    @Test
+    public void givenAnExistingPostponedEventAndMinorStatusId26WithDifferentDatesThenANewPostponedEventIsCreated() {
+        AppealCase appealWithPostponedAndMinorStatusWithDifferentDates = AppealCase.builder()
+            .appealCaseCaseCodeId("1")
+            .majorStatus(super.buildMajorStatusGivenStatuses(GapsEvent.APPEAL_RECEIVED, GapsEvent.HEARING_POSTPONED))
+            .minorStatus(super.getMinorStatusId26(TEST_DATE))
+            .build();
+
+        events = caseDataEventBuilder.buildPostponedEvent(appealWithPostponedAndMinorStatusWithDifferentDates);
+
+        assertTrue("Postponed event should be created here", events.size() == 1);
+
+        Event actualEvent = events.get(0).getValue();
+        assertTrue("event is not Postponed", actualEvent.getType().equals(
+            GapsEvent.HEARING_POSTPONED.getType()));
+
+        String existingPostponedDate = ZonedDateTime.parse(TEST_DATE2).toLocalDateTime().toString();
+        assertFalse("new postponed date cannot be equal to the existing postpone date",
+            actualEvent.getDate().equals(existingPostponedDate));
+
+        String expectedPostponedDate = appealWithPostponedAndMinorStatusWithDifferentDates
+            .getMinorStatus().get(0).getDateSet().toLocalDateTime().toString();
+        assertTrue("new postponed event date is not equal to the minor status date",
+            actualEvent.getDate().equals(expectedPostponedDate));
     }
 
     @Test
