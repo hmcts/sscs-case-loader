fddf0d392f652f9688e21ec12cd3580e6de777ac
satyachundur <>
2018-05-08 15:48:29 +0100

    fixing check style issues

--- src/e2e/java/uk.gov.hmcts.reform.sscs/functional/ProcessCaseFile.java
+++ src/e2e/java/uk.gov.hmcts.reform.sscs/functional/ProcessCaseFile.java
@@ -28,7 +28,8 @@ public class ProcessCaseFile {
     private final String caseloaderinstance = System.getenv("TEST_URL");
     String filename;
     private static final String outputdir = "src/test/resources/updates";
-    private static final String processedReference = "/incoming/processed/SSCS_Extract_Reference_2018-02-13-20-09-33.xml";
+    private static final String processedReference =
+        "/incoming/processed/SSCS_Extract_Reference_2018-02-13-20-09-33.xml";
 
     @Autowired
     private SftpChannelAdapter sftpChannelAdapter;
