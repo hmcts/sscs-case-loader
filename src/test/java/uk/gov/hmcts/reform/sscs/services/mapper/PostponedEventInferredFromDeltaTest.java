489c4ad53dda084b51573e77ae0e7a4833738a33
david.crespo <david.crespo@hmcts.net>
2018-05-08 13:19:41 +0200

    SSCS-3129 Add junit test class and Parametrised tests.

--- src/test/java/uk/gov/hmcts/reform/sscs/services/mapper/PostponedEventInferredFromDeltaTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/services/mapper/PostponedEventInferredFromDeltaTest.java
@@ -1,25 +1,33 @@
 package uk.gov.hmcts.reform.sscs.services.mapper;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Collections;
 import java.util.List;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import uk.gov.hmcts.reform.sscs.models.deserialize.gaps2.Hearing;
 import uk.gov.hmcts.reform.sscs.models.deserialize.gaps2.PostponementRequests;
 
+@RunWith(JUnitParamsRunner.class)
 public class PostponedEventInferredFromDeltaTest {
+
     @Test
-    public void givenPostponedRequestMatchesToHearingThenReturnTrue() {
+    @Parameters({"1, 1, true", "1, 2, false"})
+    public void givenPostponedRequestMatchesToHearingThenReturnTrue(String appealHearingId, String hearingId,
+                                                                    boolean expected) {
         PostponedEventInferredFromDelta postponedEventInferredFromDelta = new PostponedEventInferredFromDelta();
 
         List<PostponementRequests> postponementRequests = Collections.singletonList(
-            new PostponementRequests("Y", "1", null,
+            new PostponementRequests("Y", appealHearingId, null,
                 null)
         );
-        List<Hearing> hearingList = Collections.singletonList(Hearing.builder().hearingId("1").build());
+        List<Hearing> hearingList = Collections.singletonList(Hearing.builder().hearingId(hearingId).build());
 
         boolean actual = postponedEventInferredFromDelta.matchToHearingId(postponementRequests, hearingList);
-        assertTrue(actual);
+
+        assertEquals(expected, actual);
     }
 }
