142549829c3fe9c9c72ae40f5986ca896c8503a5
david.crespo <david.crespo@hmcts.net>
2018-05-08 02:13:10 +0200

    SSCS-3129 Add integration tests to cover reqs
    Add third test to cover scenario1.
    Add production code to make test pass.

--- src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/services/mapper/CaseDataEventBuilderTest.java
+++ src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/services/mapper/CaseDataEventBuilderTest.java
@@ -3,12 +3,6 @@ package uk.gov.hmcts.reform.sscs.services.mapper;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Matchers.anyListOf;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
@@ -23,7 +17,6 @@ import java.util.List;
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Matchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -36,7 +29,6 @@ import uk.gov.hmcts.reform.sscs.models.GapsEvent;
 import uk.gov.hmcts.reform.sscs.models.deserialize.gaps2.AppealCase;
 import uk.gov.hmcts.reform.sscs.models.deserialize.gaps2.Hearing;
 import uk.gov.hmcts.reform.sscs.models.deserialize.gaps2.PostponementRequests;
-import uk.gov.hmcts.reform.sscs.models.idam.IdamTokens;
 import uk.gov.hmcts.reform.sscs.models.serialize.ccd.Events;
 import uk.gov.hmcts.reform.sscs.services.idam.IdamService;
 import uk.gov.hmcts.reform.sscs.services.sftp.SftpChannelAdapter;
@@ -102,7 +94,7 @@ public class CaseDataEventBuilderTest extends CaseDataBuilderBaseTest {
             coreCaseDataProperties.getJurisdictionId(),
             coreCaseDataProperties.getCaseTypeId(),
             ImmutableMap.of("case.caseReference", appeal.getAppealCaseRefNum())
-        )).willReturn(Collections.singletonList(getCaseDetails(CASE_DETAILS_WITH_HEARINGS_JSON)));
+        )).willReturn(Collections.singletonList(getCaseDetails()));
 
         events = caseDataEventBuilder.buildPostponedEvent(appeal);
 
@@ -149,9 +141,63 @@ public class CaseDataEventBuilderTest extends CaseDataBuilderBaseTest {
         assertEquals(expectedDate, actualPostponedDate);
     }
 
+    /*
+      scenario3:
+      Given minor status with id 27
+      And multiple hearing objects
+      And two postponed request elements with the granted field to 'Y'
+      And one of them matching the hearing id field to the hearing in the existing Case in CDD
+      Then one postponed element is created
+   */
+    @Test
+    public void givenScenario3ThenPostponedIsCreated()
+        throws Exception {
+        AppealCase appeal = AppealCase.builder()
+            .appealCaseCaseCodeId("1")
+            .appealCaseRefNum("SC068/17/00011")
+            .majorStatus(Collections.singletonList(
+                super.buildMajorStatusGivenStatusAndDate(GapsEvent.APPEAL_RECEIVED.getStatus(), APPEAL_RECEIVED_DATE)
+            ))
+            .minorStatus(Collections.singletonList(
+                super.buildMinorStatusGivenIdAndDate("27", MINOR_STATUS_ID_27_DATE)))
+            .hearing(Arrays.asList(
+                Hearing.builder().hearingId("1").build(),
+                Hearing.builder().hearingId("2").build()
+            ))
+            .postponementRequests(Arrays.asList(
+                new PostponementRequests(
+                    "Y", "6", null, null),
+                new PostponementRequests(
+                    "Y", "", null, null)
+            ))
+            .build();
 
-    private static CaseDetails getCaseDetails(String caseDetails) throws IOException {
-        String caseDetailsWithHearings = FileUtils.readFileToString(new File(caseDetails),
+
+        given(idamService.getIdamOauth2Token()).willReturn("oauth2Token");
+        given(idamService.generateServiceAuthorization()).willReturn("serviceToken");
+
+        given(coreCaseDataApi.searchForCaseworker(
+            "oauth2Token",
+            "serviceToken",
+            coreCaseDataProperties.getUserId(),
+            coreCaseDataProperties.getJurisdictionId(),
+            coreCaseDataProperties.getCaseTypeId(),
+            ImmutableMap.of("case.caseReference", appeal.getAppealCaseRefNum())
+        )).willReturn(Collections.singletonList(getCaseDetails()));
+
+        events = caseDataEventBuilder.buildPostponedEvent(appeal);
+
+        assertEquals("One postponed event expected here", 1, events.size());
+        assertEquals("type expected is postponed", GapsEvent.HEARING_POSTPONED.getType(),
+            events.get(0).getValue().getType());
+        LocalDateTime actualPostponedDate = LocalDateTime.parse(events.get(0).getValue().getDate());
+        LocalDateTime expectedDate = ZonedDateTime.parse(MINOR_STATUS_ID_27_DATE).toLocalDateTime();
+        assertEquals(expectedDate, actualPostponedDate);
+    }
+
+    private static CaseDetails getCaseDetails() throws IOException {
+        String caseDetailsWithHearings = FileUtils.readFileToString(
+            new File(CaseDataEventBuilderTest.CASE_DETAILS_WITH_HEARINGS_JSON),
             StandardCharsets.UTF_8.name());
         ObjectMapper mapper = Jackson2ObjectMapperBuilder.json().build();
         return mapper.readerFor(CaseDetails.class).readValue(caseDetailsWithHearings);
