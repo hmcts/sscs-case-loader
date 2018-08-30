21de65a3a79f7d526a4be693641939508459fd3a
david.crespo <david.crespo@hmcts.net>
2018-08-30 14:37:07 +0200

    Revert "[PREVIEW]test new idam dns in prod (#216)"
    This reverts commit 08bb2caef09b11bc653ebe90b952058b85151f18.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -80,7 +80,7 @@ module "sscs-case-loader" {
     CORE_CASE_DATA_JURISDICTION_ID = "${var.core_case_data_jurisdiction_id}"
     CORE_CASE_DATA_CASE_TYPE_ID = "${var.core_case_data_case_type_id}"
 
-    IDAM_URL = "${var.env == "prod" ? "https://idam-api.platform.hmcts.net" : data.vault_generic_secret.idam_api.data["value"]}"
+    IDAM_URL = "${data.vault_generic_secret.idam_api.data["value"]}"
 
     IDAM.S2S-AUTH.TOTP_SECRET = "${data.vault_generic_secret.sscs_s2s_secret.data["value"]}"
     IDAM.S2S-AUTH = "${local.s2sCnpUrl}"
