cd57f8be2e9d842c8449e24f291f8d60aa25b2fb
david.crespo <david.crespo@hmcts.net>
2018-11-28 16:46:08 +0000

    Refactor testing code to make cleaner the parameters code

--- src/test/java/uk/gov/hmcts/reform/sscs/services/ccd/UpdateCcdAppellantDataTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/services/ccd/UpdateCcdAppellantDataTest.java
@@ -7,7 +7,6 @@ import static uk.gov.hmcts.reform.sscs.CaseDetailsUtils.getSscsCaseDetails;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
-import junitparams.converters.Nullable;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.gov.hmcts.reform.sscs.ccd.domain.Appeal;
@@ -53,45 +52,41 @@ public class UpdateCcdAppellantDataTest {
     }
 
     @Test
-    @Parameters({
-        "first-name, first-name, last-name, last-name, email@email.com, email@email.com, AB46575S, AB46575S,"
-            + "existingCaseEmail@email.com, existingFirstName, existingLastName, CA 36 98 74 A",
-        ", existingFirstName,, existingLastName,, existingCaseEmail@email.com,, CA 36 98 74 A, "
-            + "existingCaseEmail@email.com, existingFirstName, existingLastName, CA 36 98 74 A",
-        "null, existingFirstName, null, existingLastName, null, existingCaseEmail@email.com, null, CA 36 98 74 A, "
-            + "existingCaseEmail@email.com, existingFirstName, existingLastName, CA 36 98 74 A",
-        "null, existingFirstName, null, existingLastName, email@email.com, email@email.com, null, CA 36 98 74 A, null,"
-            + "existingFirstName, existingLastName, CA 36 98 74 A",
-        "null, existingFirstName, null, existingLastName, email@email.com, email@email.com, null, CA 36 98 74 A,,"
-            + "existingFirstName, existingLastName, CA 36 98 74 A",
-        "null, existingFirstName, null, existingLastName,,null, null, CA 36 98 74 A, null, existingFirstName, "
-            + "existingLastName, CA 36 98 74 A",
-        "first-name, first-name, null, existingLastName,,null, null, CA 36 98 74 A, null,, existingLastName, "
-            + "CA 36 98 74 A",
-        "first-name, first-name, null, existingLastName,,null, null, CA 36 98 74 A, null, null, existingLastName, "
-            + "CA 36 98 74 A",
-        "first-name, first-name, null, existingLastName,,null,,, null, null, existingLastName,",
-        "first-name, first-name, null, existingLastName,,null,null,null, null, null, existingLastName, null"
-    })
+//    @Parameters({
+//        "first-name, first-name, last-name, last-name, email@email.com, email@email.com, AB46575S, AB46575S,"
+//            + "existingCaseEmail@email.com, existingFirstName, existingLastName, CA 36 98 74 A",
+//        ", existingFirstName,, existingLastName,, existingCaseEmail@email.com,, CA 36 98 74 A, "
+//            + "existingCaseEmail@email.com, existingFirstName, existingLastName, CA 36 98 74 A",
+//        "null, existingFirstName, null, existingLastName, null, existingCaseEmail@email.com, null, CA 36 98 74 A, "
+//            + "existingCaseEmail@email.com, existingFirstName, existingLastName, CA 36 98 74 A",
+//        "null, existingFirstName, null, existingLastName, email@email.com, email@email.com, null, CA 36 98 74 A, null,"
+//            + "existingFirstName, existingLastName, CA 36 98 74 A",
+//        "null, existingFirstName, null, existingLastName, email@email.com, email@email.com, null, CA 36 98 74 A,,"
+//            + "existingFirstName, existingLastName, CA 36 98 74 A",
+//        "null, existingFirstName, null, existingLastName,,null, null, CA 36 98 74 A, null, existingFirstName, "
+//            + "existingLastName, CA 36 98 74 A",
+//        "first-name, first-name, null, existingLastName,,null, null, CA 36 98 74 A, null,, existingLastName, "
+//            + "CA 36 98 74 A",
+//        "first-name, first-name, null, existingLastName,,null, null, CA 36 98 74 A, null, null, existingLastName, "
+//            + "CA 36 98 74 A",
+//        "first-name, first-name, null, existingLastName,,null,,, null, null, existingLastName,",
+//        "first-name, first-name, null, existingLastName,,null,null,null, null, null, existingLastName, null"
+//    })
+    @Parameters(method = "generateUpdateCaseDataScenarios")
     public void givenAppellantUpdatesInGapsData_shouldUpdateExistingCcdAppellantData(
-        @Nullable String gapsFirstName, @Nullable String expectedExistingCcdFirstName,
-        @Nullable String gapsLastName, @Nullable String expectedExistingCcdLastName,
-        @Nullable String gapsContactEmail, @Nullable String expectedExistingCcdContactEmail,
-        @Nullable String gapsNino, @Nullable String expectedExistingCcdNino,
-        @Nullable String existingCcdContactEmail, @Nullable String existingCddFirstName,
-        @Nullable String existingCcdLastName, @Nullable String existingCcdNino) throws Exception {
+        GapsAndCcdDataUpdateScenario gapsAndCcdDataUpdateScenario) throws Exception {
 
         Appellant appellant = Appellant.builder()
             .name(Name.builder()
-                .firstName(gapsFirstName)
-                .lastName(gapsLastName)
+                .firstName(gapsAndCcdDataUpdateScenario.gapsAppellantData.firstName)
+                .lastName(gapsAndCcdDataUpdateScenario.gapsAppellantData.lastName)
                 .title("Mr")
                 .build())
             .contact(Contact.builder()
-                .email(gapsContactEmail)
+                .email(gapsAndCcdDataUpdateScenario.gapsAppellantData.contactEmail)
                 .build())
             .identity(Identity.builder()
-                .nino(gapsNino)
+                .nino(gapsAndCcdDataUpdateScenario.gapsAppellantData.nino)
                 .build())
             .build();
 
@@ -104,21 +99,105 @@ public class UpdateCcdAppellantDataTest {
         gapsCaseData.getAppeal().setAppellant(appellant);
 
         existingCaseDetails = getSscsCaseDetails(CcdCasesSenderTest.CASE_DETAILS_JSON);
-        existingCaseDetails.getData().getAppeal().getAppellant().getContact().setEmail(existingCcdContactEmail);
-        existingCaseDetails.getData().getAppeal().getAppellant().getName().setFirstName(existingCddFirstName);
-        existingCaseDetails.getData().getAppeal().getAppellant().getName().setLastName(existingCcdLastName);
-        existingCaseDetails.getData().getAppeal().getAppellant().getIdentity().setNino(existingCcdNino);
+        existingCaseDetails.getData().getAppeal().getAppellant().getContact().setEmail(
+            gapsAndCcdDataUpdateScenario.existingCcdAppellantData.contactEmail);
+        existingCaseDetails.getData().getAppeal().getAppellant().getName().setFirstName(
+            gapsAndCcdDataUpdateScenario.existingCcdAppellantData.firstName);
+        existingCaseDetails.getData().getAppeal().getAppellant().getName().setLastName(
+            gapsAndCcdDataUpdateScenario.existingCcdAppellantData.lastName);
+        existingCaseDetails.getData().getAppeal().getAppellant().getIdentity().setNino(
+            gapsAndCcdDataUpdateScenario.existingCcdAppellantData.nino);
 
         updateCcdAppellantData.updateCcdAppellantData(gapsCaseData, existingCaseDetails.getData());
 
         assertThat(existingCaseDetails.getData().getAppeal().getAppellant().getName().getFirstName(),
-            equalTo(expectedExistingCcdFirstName));
+            equalTo(gapsAndCcdDataUpdateScenario.expectedExistingCcdAppellantName.firstName));
         assertThat(existingCaseDetails.getData().getAppeal().getAppellant().getName().getLastName(),
-            equalTo(expectedExistingCcdLastName));
+            equalTo(gapsAndCcdDataUpdateScenario.expectedExistingCcdAppellantName.lastName));
         assertThat(existingCaseDetails.getData().getAppeal().getAppellant().getContact().getEmail(),
-            equalTo(expectedExistingCcdContactEmail));
+            equalTo(gapsAndCcdDataUpdateScenario.expectedExistingCcdAppellantName.contactEmail));
         assertThat(existingCaseDetails.getData().getAppeal().getAppellant().getIdentity().getNino(),
-            equalTo(expectedExistingCcdNino));
+            equalTo(gapsAndCcdDataUpdateScenario.expectedExistingCcdAppellantName.nino));
     }
 
+    @SuppressWarnings("PMD.UnusedPrivateMethod")
+    private Object[] generateUpdateCaseDataScenarios() {
+        GapsAndCcdDataUpdateScenario gapsAndCcdDataUpdateScenario =
+            updateCcdDataWhenThereAreGapsDataUpdatesHappyPaths();
+
+        return new Object[]{
+            new Object[]{gapsAndCcdDataUpdateScenario}
+        };
+    }
+
+    private GapsAndCcdDataUpdateScenario updateCcdDataWhenThereAreGapsDataUpdatesHappyPaths() {
+        GapsAppellantData gapsAppellantData = new GapsAppellantData(
+            "first-name", "last-name", "email@email.com", "AB46575S");
+
+        ExpectedExistingCcdAppellantName expectedExistingCcdAppellantName =
+            new ExpectedExistingCcdAppellantName("first-name", "last-name",
+                "email@email.com", "AB46575S");
+
+        ExistingCcdAppellantData existingCcdAppellantData = new ExistingCcdAppellantData(
+            "existingFirstName", "existingLastName", "existingCaseEmail@email.com",
+            "CA 36 98 74 A");
+        return new GapsAndCcdDataUpdateScenario(
+            gapsAppellantData, expectedExistingCcdAppellantName, existingCcdAppellantData);
+    }
+
+    private class GapsAppellantData {
+        private final String firstName;
+        private final String lastName;
+        private final String contactEmail;
+        private final String nino;
+
+        private GapsAppellantData(String firstName, String lastName, String contactEmail, String nino) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.contactEmail = contactEmail;
+            this.nino = nino;
+        }
+    }
+
+    private class ExpectedExistingCcdAppellantName {
+        private final String firstName;
+        private final String lastName;
+        private final String contactEmail;
+        private final String nino;
+
+        private ExpectedExistingCcdAppellantName(String firstName, String lastName, String contactEmail, String nino) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.contactEmail = contactEmail;
+            this.nino = nino;
+        }
+    }
+
+    private class ExistingCcdAppellantData {
+        private final String firstName;
+        private final String lastName;
+        private final String contactEmail;
+        private final String nino;
+
+        private ExistingCcdAppellantData(String firstName, String lastName, String contactEmail, String nino) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.contactEmail = contactEmail;
+            this.nino = nino;
+        }
+    }
+
+    private class GapsAndCcdDataUpdateScenario {
+        private final GapsAppellantData gapsAppellantData;
+        private final ExpectedExistingCcdAppellantName expectedExistingCcdAppellantName;
+        private final ExistingCcdAppellantData existingCcdAppellantData;
+
+        private GapsAndCcdDataUpdateScenario(GapsAppellantData gapsAppellantData,
+                                             ExpectedExistingCcdAppellantName expectedExistingCcdAppellantName,
+                                             ExistingCcdAppellantData existingCcdAppellantData) {
+            this.gapsAppellantData = gapsAppellantData;
+            this.expectedExistingCcdAppellantName = expectedExistingCcdAppellantName;
+            this.existingCcdAppellantData = existingCcdAppellantData;
+        }
+    }
 }
