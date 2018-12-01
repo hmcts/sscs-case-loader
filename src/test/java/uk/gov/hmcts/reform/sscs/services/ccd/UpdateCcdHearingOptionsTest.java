84fb1810a82c58801c79d5b628973226c72e0e02
david.crespo <david.crespo@hmcts.net>
2018-12-01 01:51:43 +0100

    Parametrise the test to cover other scenarios reusing the same code.

--- src/test/java/uk/gov/hmcts/reform/sscs/services/ccd/UpdateCcdHearingOptionsTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/services/ccd/UpdateCcdHearingOptionsTest.java
@@ -1,19 +1,36 @@
 package uk.gov.hmcts.reform.sscs.services.ccd;
 
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import uk.gov.hmcts.reform.sscs.ccd.domain.Appeal;
 import uk.gov.hmcts.reform.sscs.ccd.domain.HearingOptions;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 
+@RunWith(JUnitParamsRunner.class)
 public class UpdateCcdHearingOptionsTest {
     @Test
-    public void givenGapsWantsToAttendChange_shouldUpdateExistingCcdHearingOptions() {
+    @Parameters(method = "generateHearingOptionUpdateScenarios")
+    public void givenGapsWantsToAttendChange_shouldUpdateExistingCcdHearingOptions(SscsCaseData gapsCaseData,
+                                                                                   SscsCaseData existingCcdCaseData,
+                                                                                   boolean expectedUpdateData,
+                                                                                   String expectedWantsToAttend) {
         UpdateCcdHearingOptions updateCcdHearingOptions = new UpdateCcdHearingOptions();
 
+        boolean updateData = updateCcdHearingOptions.updateHearingOptions(gapsCaseData, existingCcdCaseData);
+
+        assertEquals(expectedUpdateData, updateData);
+        assertThat(existingCcdCaseData.getAppeal().getHearingOptions().getWantsToAttend(), is(expectedWantsToAttend));
+    }
+
+
+    @SuppressWarnings("PMD.UnusedPrivateMethod")
+    private Object[] generateHearingOptionUpdateScenarios() {
         SscsCaseData gapsCaseData = SscsCaseData.builder()
             .appeal(Appeal.builder()
                 .hearingOptions(HearingOptions.builder()
@@ -30,9 +47,8 @@ public class UpdateCcdHearingOptionsTest {
                 .build())
             .build();
 
-        boolean updateData = updateCcdHearingOptions.updateHearingOptions(gapsCaseData, existingCcdCaseData);
-
-        assertTrue(updateData);
-        assertThat(existingCcdCaseData.getAppeal().getHearingOptions().getWantsToAttend(), is("yes"));
+        return new Object[]{
+            new Object[]{gapsCaseData, existingCcdCaseData, true, "yes"}
+        };
     }
 }
