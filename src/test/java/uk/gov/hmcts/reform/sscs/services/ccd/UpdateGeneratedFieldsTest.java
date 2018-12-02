d4bebe41e7c344d440f500dd8af537235ac20b40
david.crespo <david.crespo@hmcts.net>
2018-12-02 01:45:51 +0100

    Add coverage for generatedSurname happy paths

--- src/test/java/uk/gov/hmcts/reform/sscs/services/ccd/UpdateGeneratedFieldsTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/services/ccd/UpdateGeneratedFieldsTest.java
@@ -1,26 +1,33 @@
 package uk.gov.hmcts.reform.sscs.services.ccd;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.gov.hmcts.reform.sscs.ccd.domain.Appeal;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Appellant;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Name;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 
 @RunWith(JUnitParamsRunner.class)
 public class UpdateGeneratedFieldsTest {
+
+    private final UpdateGeneratedFields updateGeneratedFields = new UpdateGeneratedFields();
+
     @Test
     @Parameters(method = "generateInvalidScenarios")
     public void givenInvalidExistingCcdCaseData_shouldNotGenerateFields(SscsCaseData existingCcdCaseData,
                                                                         SscsCaseData expectedExistingCcdCaseData) {
-        UpdateGeneratedFields updateGeneratedFields = new UpdateGeneratedFields();
         updateGeneratedFields.updateGeneratedFields(existingCcdCaseData);
         assertEquals(expectedExistingCcdCaseData, existingCcdCaseData);
     }
 
-    public Object[] generateInvalidScenarios() {
+    @SuppressWarnings("PMD.UnusedPrivateMethod")
+    private Object[] generateInvalidScenarios() {
         SscsCaseData existingCaseDataWithNullAppeal = SscsCaseData.builder()
             .build();
         SscsCaseData existingCaseDataWithNullAppellant = SscsCaseData.builder()
@@ -32,5 +39,33 @@ public class UpdateGeneratedFieldsTest {
             new Object[]{existingCaseDataWithNullAppellant, existingCaseDataWithNullAppellant}
         };
     }
+
+    @Test
+    @Parameters(method = "generateAppellantScenarios")
+    public void givenValidData_shouldUpdateAppellantName(Appellant appellant) {
+        SscsCaseData existingCcdCaseData = SscsCaseData.builder()
+            .appeal(Appeal.builder()
+                .appellant(appellant)
+                .build())
+            .build();
+
+        updateGeneratedFields.updateGeneratedFields(existingCcdCaseData);
+
+        String expectedLastName = existingCcdCaseData.getAppeal().getAppellant().getName().getLastName();
+        assertThat(existingCcdCaseData.getGeneratedSurname(), equalTo(expectedLastName));
+    }
+
+    @SuppressWarnings("PMD.UnusedPrivateMethod")
+    private Object[] generateAppellantScenarios() {
+        Appellant appellant = Appellant.builder()
+            .name(Name.builder()
+                .lastName("lastName")
+                .build())
+            .build();
+        return new Object[]{
+            new Object[]{appellant}
+        };
+    }
+
 
 }
