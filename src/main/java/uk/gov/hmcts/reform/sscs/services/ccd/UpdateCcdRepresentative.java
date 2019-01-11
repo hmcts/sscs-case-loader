c30252a0dd45073d13de0d2a70b05958a38021ff
srochani <sunil.rochani@hotmail.com>
2019-01-11 15:45:19 +0000

    [PREVIEW] SSCS-4919 Case Loader Exception (unprocessable entity) (#325)
    * [PREVIEW] SSCS-4919 Case Loader Exception (unprocessable entity)
    
    * [PREVIEW] SSCS-4919 Case Loader Exception (unprocessable entity)
    
    * Add more junit coverage for other edge scenarios
    
    when Reps data such as contact or Name or Address comes empty or null
    Then we don't overwrite the existing data
    
    * Add some more edge scenarios
    
    * Fix PMD and checkstyle issues

--- src/main/java/uk/gov/hmcts/reform/sscs/services/ccd/UpdateCcdRepresentative.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/services/ccd/UpdateCcdRepresentative.java
@@ -1,10 +1,14 @@
 package uk.gov.hmcts.reform.sscs.services.ccd;
 
+import lombok.extern.slf4j.Slf4j;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Contact;
 import uk.gov.hmcts.reform.sscs.ccd.domain.Representative;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.domain.Subscription;
 import uk.gov.hmcts.reform.sscs.ccd.domain.Subscriptions;
+import uk.gov.hmcts.reform.sscs.util.UkMobile;
 
+@Slf4j
 final class UpdateCcdRepresentative {
     private static final String YES = "Yes";
     private static final String NO = "No";
@@ -23,12 +27,47 @@ final class UpdateCcdRepresentative {
                 || !existingRepresentative.getName().equals(rep.getName())
                 || !existingRepresentative.getContact().equals(rep.getContact())
             ) {
-                existingCcdCaseData.getAppeal().setRep(rep);
+                updateReps(gapsCaseData, existingCcdCaseData, rep);
                 updateRepresentativeSubscription(gapsCaseData, existingCcdCaseData);
                 repUpdated = true;
             }
         }
         return repUpdated;
+    }
+
+    private static void updateReps(SscsCaseData gapsCaseData, SscsCaseData existingCcdCaseData, Representative rep) {
+        if (existingCcdCaseData.getAppeal().getRep() == null) {
+            existingCcdCaseData.getAppeal().setRep(gapsCaseData.getAppeal().getRep());
+        } else {
+            if (rep.getContact() != null) {
+                updateContact(gapsCaseData.getCaseReference(), existingCcdCaseData, rep);
+            }
+            if (rep.getName() != null) {
+                existingCcdCaseData.getAppeal().getRep().setName(rep.getName());
+            }
+            if (rep.getAddress() != null) {
+                existingCcdCaseData.getAppeal().getRep().setAddress(rep.getAddress());
+            }
+        }
+    }
+
+    private static void updateContact(String caseRef, SscsCaseData existingCcdCaseData, Representative rep) {
+        String mobileNumber;
+        if (UkMobile.validate(rep.getContact().getMobile())) {
+            mobileNumber = rep.getContact().getMobile();
+        } else {
+            log.info("Invalid Uk mobile no: {} for the case reference: {}",
+                rep.getContact().getMobile(), caseRef);
+            Contact existingRepsContact = existingCcdCaseData.getAppeal().getRep().getContact();
+            mobileNumber = existingRepsContact != null ? existingRepsContact.getMobile() : null;
+        }
+        Contact contact = Contact.builder()
+            .email(rep.getContact().getEmail())
+            .phone(rep.getContact().getPhone())
+            .mobile(mobileNumber)
+            .build();
+
+        existingCcdCaseData.getAppeal().getRep().setContact(contact);
     }
 
     private static void updateRepresentativeSubscription(SscsCaseData gapsCaseData, SscsCaseData existingCcdCaseData) {
@@ -51,7 +90,7 @@ final class UpdateCcdRepresentative {
     }
 
     private static Subscription keepExistingSubscribedSubscriptions(Subscription newRepSubscription,
-                                                     Subscription existingRepSubscription) {
+                                                                    Subscription existingRepSubscription) {
         return newRepSubscription.toBuilder()
             .subscribeSms(existingRepSubscription != null && existingRepSubscription.isSmsSubscribed() ? YES : NO)
             .subscribeEmail(existingRepSubscription != null && existingRepSubscription.isEmailSubscribed() ? YES : NO)
