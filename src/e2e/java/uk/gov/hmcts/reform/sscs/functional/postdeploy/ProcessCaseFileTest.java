3edb4433e7fac0f78bb3863e2c478cffeada3375
Chris Moreton <chris@netsensia.com>
2019-10-14 10:51:26 +0100

    Breaking a test to test the pipeline :)

--- src/e2e/java/uk/gov/hmcts/reform/sscs/functional/postdeploy/ProcessCaseFileTest.java
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/postdeploy/ProcessCaseFileTest.java
@@ -59,7 +59,7 @@ public class ProcessCaseFileTest {
 
         SscsCaseData updatedCcdCaseData = updatedCcdCase.getData();
 
-        assertEquals("XYZ", updatedCcdCaseData.getAppeal().getAppellant().getName().getFirstName());
+        assertEquals("ABS", updatedCcdCaseData.getAppeal().getAppellant().getName().getFirstName());
         assertEquals(3, updatedCcdCaseData.getEvents().size());
         assertEquals("appealCreated", updatedCcdCase.getState());
     }
