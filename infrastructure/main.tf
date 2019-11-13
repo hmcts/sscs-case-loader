f6e3b3196ee14e63ed6326a249b7603218890247
david.crespo <david.crespo@hmcts.net>
2019-11-13 10:32:45 +0100

    rename s2s url env var to match the configuration in bootstrap and values.yaml

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -97,7 +97,7 @@ module "sscs-case-loader" {
     IDAM_URL = "${data.azurerm_key_vault_secret.idam-api.value}"
 
     IDAM_S2S_AUTH_TOTP_SECRET  = "${data.azurerm_key_vault_secret.sscs-s2s-secret.value}"
-    IDAM.S2S-AUTH              = "${local.s2sCnpUrl}"
+    IDAM_S2S_AUTH              = "${local.s2sCnpUrl}"
     IDAM_S2S_AUTH_MICROSERVICE = "${var.idam_s2s_auth_microservice}"
 
     IDAM_SSCS_SYSTEMUPDATE_USER     = "${data.azurerm_key_vault_secret.idam-sscs-systemupdate-user.value}"
