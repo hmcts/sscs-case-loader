ab31a09a62c41a192cb95ab50fb46c08c6f2e282
david.crespo <david.crespo@hmcts.net>
2019-11-13 10:04:50 +0100

    clean up old configuration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -113,10 +113,7 @@ module "sscs-case-loader" {
     GAPS2_SFTP_USER    = "${var.gaps2_sftp_user}"
     GAPS2_SFTP_DIR     = "${var.gaps2_sftp_dir}"
 
-    SSCS_CASE_LOADER_CRON_SCHEDULE = "${var.sscs_case_loader_cron_schedule}"
     IGNORE_CASES_BEFORE_DATE       = "${var.ignore_cases_before_date}"
-
-    NUMBER_PROCESSED_CASES_TO_REFRESH_TOKENS = "${var.number_processed_cases_to_refresh_tokens}"
 
     # addtional log
     ROOT_LOGGING_LEVEL   = "${var.root_logging_level}"
--- infrastructure/prod.tfvars
+++ infrastructure/prod.tfvars
@@ -1,6 +1,5 @@
 infrastructure_env = "prod"
 gaps2_sftp_user = "sscs-sftp-prod"
-sscs_case_loader_cron_schedule = "0 0 * * * *"
 ignore_cases_before_date = "2018-07-26"
 sftp_key_location = "gaps2_service_sftp_key"
 idam_redirect_url = "https://sscs-case-loader-prod.service.core-compute-prod.internal"
