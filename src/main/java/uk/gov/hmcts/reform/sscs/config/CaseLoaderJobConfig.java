2ce300c845bd31b25e4cb3b4ad967cda5f0ad74e
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-11-20 21:47:27 +0000

    pmd

--- src/main/java/uk/gov/hmcts/reform/sscs/config/CaseLoaderJobConfig.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/CaseLoaderJobConfig.java
@@ -33,8 +33,8 @@ public class CaseLoaderJobConfig {
 
     @Bean
     public Step caseLoaderStep() {
-        return stepBuilders.get("caseLoaderStep").
-            tasklet(tasklet())
+        return stepBuilders.get("caseLoaderStep")
+            .tasklet(tasklet())
             .build();
     }
 
