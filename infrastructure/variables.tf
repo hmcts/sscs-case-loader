fbd6bf190381750ea04f159cbf8e9ca60643b339
jackmaloney <jack.maloney@transformuk.com>
2019-11-20 11:28:16 +0000

    Enable ASE on demo only

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -108,3 +108,7 @@ variable "appinsights_instrumentation_key" {
   description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
   default     = ""
 }
+
+variable "enable_ase" {
+  default = false
+}
