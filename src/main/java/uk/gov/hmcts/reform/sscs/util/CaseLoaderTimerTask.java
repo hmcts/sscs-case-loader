eddd9d7bb9d77503c72443d0bb7485f3019eb4cf
srochani <sunil.rochani@hotmail.com>
2019-11-28 15:16:39 +0000

    SSCS-6963 Caseloader – Ensure all logs are recorded before shutting down

--- src/main/java/uk/gov/hmcts/reform/sscs/util/CaseLoaderTimerTask.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/util/CaseLoaderTimerTask.java
@@ -2,14 +2,14 @@ package uk.gov.hmcts.reform.sscs.util;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.TimerTask;
 
 @Slf4j
 public class CaseLoaderTimerTask extends TimerTask {
     @Override
     public void run() {
-        log.info("Case loader shutdown performed on: " + new Date() +
+        log.info("Case loader shutdown performed on: " + LocalDateTime.now() +
                 " Thread's name: " + Thread.currentThread().getName());
     }
 }
