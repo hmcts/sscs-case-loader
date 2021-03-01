acfc678319245aa5b2067b7f7b19c973c8eba940
srochani <sunil.rochani@hotmail.com>
2021-03-01 11:10:38 +0000

    Sscs 8639 cs amendpostponedlogic (#929)
    * SSCS-8418: Caseloader - Modify logic on search cases if multiple SC number then search of case id
    
    * SSCS-8639: Caseloader – Amend hearingPostponed logic
    
    * SSCS-8639: Caseloader – Amend hearingPostponed logic

--- src/test/java/uk/gov/hmcts/reform/sscs/services/mapper/CaseDataBuilderBase.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/services/mapper/CaseDataBuilderBase.java
@@ -10,6 +10,7 @@ class CaseDataBuilderBase {
     static final String MINOR_STATUS_ID_27_DATE = "2018-05-24T00:00:00+01:00";
     static final String APPEAL_RECEIVED_DATE = "2017-05-24T00:00:00+01:00";
     static final String RESPONSE_RECEIVED_DATE = "2017-06-24T00:00:00+01:00";
+    static final String APPEAL_RECEIVED_DATE_GREATER_THAN_IGNORE_DATE = "2021-02-24T00:00:00+01:00";
 
     MajorStatus buildMajorStatusGivenStatusAndDate(String status, String testDate) {
         return new MajorStatus("", status, null, ZonedDateTime.parse(testDate));
