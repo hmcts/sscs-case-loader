c305687fb6ab2f1fccdb4be88454ab76d12854f9
nickhill111 <nickhill11@live.co.uk>
2023-03-16 16:35:04 +0000

    add correct asserts SSCS-common

--- src/test/java/uk/gov/hmcts/reform/sscs/services/ccd/CcdCasesSenderTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/services/ccd/CcdCasesSenderTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.sscs.services.ccd;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.*;
 import static org.mockito.BDDMockito.given;
