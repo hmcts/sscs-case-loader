ccb7b7114671bbed9002adbcc196f29c3df4c420
nickhill111 <nickhill11@live.co.uk>
2023-03-17 13:44:42 +0000

    remove unused imports SSCS-common

--- src/main/java/uk/gov/hmcts/reform/sscs/CaseLoaderApp.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/CaseLoaderApp.java
@@ -1,10 +1,8 @@
 package uk.gov.hmcts.reform.sscs;
 
-import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
