7767b0527712f42793dceb9377a7ae20a30dab3e
Mohammed Ali <mohammed.ali@hmcts.net>
2023-06-22 17:03:53 +0100

    Update build.gradle

--- src/main/java/uk/gov/hmcts/reform/sscs/CaseLoaderApp.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/CaseLoaderApp.java
@@ -5,7 +5,6 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +13,6 @@ import uk.gov.hmcts.reform.sscs.ccd.config.CcdRequestDetails;
 
 @EnableBatchProcessing
 @SpringBootApplication
-@EnableHystrixDashboard
 @SuppressWarnings("HideUtilityClassConstructor")
 @EnableFeignClients(basePackages = {
     "uk.gov.hmcts.reform.idam",
