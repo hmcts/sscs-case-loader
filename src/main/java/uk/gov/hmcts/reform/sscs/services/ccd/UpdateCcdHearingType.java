be9bcf7142aa177665615378e921be43fae2fd77
Gemma Talbot <gemma.talbot@solirius.com>
2024-01-24 13:01:05 +0000

    update hearing types

--- src/main/java/uk/gov/hmcts/reform/sscs/services/ccd/UpdateCcdHearingType.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/services/ccd/UpdateCcdHearingType.java
@@ -1,10 +1,13 @@
 package uk.gov.hmcts.reform.sscs.services.ccd;
 
+import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.ccd.domain.HearingType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 
+@Slf4j
 @Service
 class UpdateCcdHearingType {
     boolean updateHearingType(SscsCaseData gapsCaseData,
@@ -16,7 +19,12 @@ class UpdateCcdHearingType {
                 if (!HearingType.COR.getValue().equalsIgnoreCase(ccdHearingType)
                     && !HearingType.DOMICILIARY.getValue().equalsIgnoreCase(gaps2HearingType)) {
                     if (!gaps2HearingType.equals(ccdHearingType)) {
-                        existingCcdCaseData.getAppeal().setHearingType(gaps2HearingType);
+                        try {
+                            existingCcdCaseData.getAppeal().setHearingType(gaps2HearingType);
+                        } catch (FeignException e) {
+                            log.info("Could not update hearing type from {} to {} for case {}", gaps2HearingType,
+                                ccdHearingType, existingCcdCaseData.getCcdCaseId());
+                        }
                         return true;
                     }
                 }
