dbc80cf148be491170dd3181dd85f8cd99c1b621
KatKovacs1 <122097259+KatKovacs1@users.noreply.github.com>
2025-02-19 09:53:20 +0000

    SSCSCI-1623: AzureRM bump to 4.15.0 (#1572)
    * suppression update
    
    * terraform bump to 1.10.5
    
    * azurerm bump to 4.15.0
    
    ---------
    
    Co-authored-by: Katalin Kovacs <katalin.kovacs@solirius.com>

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.33"
+      version = "~> 4.15.0"
     }
     random = {
       source = "hashicorp/random"
