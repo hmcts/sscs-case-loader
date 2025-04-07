069968ad97e0a19ccfadc87aba8da4fc4170c7d4
taleb <20416858+benouaer@users.noreply.github.com>
2025-04-07 14:59:05 +0100

    give test more accurate name

--- src/test/java/uk/gov/hmcts/reform/sscs/SmokeTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/SmokeTest.java
@@ -29,7 +29,7 @@ public class SmokeTest {
     }
 
     @Test
-    public void shouldExecuteCaseLoaderJob() {
+    public void shouldSuccessfullyStartupAndExit() {
         underTest.execute(null, null);
     }
 }
