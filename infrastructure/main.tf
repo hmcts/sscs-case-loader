a5f4feceeb6a1ae8c24d0d99d27509ba55c2c014
manohar.nunna <manohar.nunna@hmcts.net>
2026-05-01 09:42:29 +0100

    DTSPO-30516: add Jenkins MI key vault access policy for sscs-case-loader

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,3 +11,61 @@ resource "azurerm_resource_group" "rg" {
     tomap({ "lastUpdated" = "${timestamp()}" })
   ))
 }
+
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
+data "azurerm_key_vault" "case_loader" {
+  name                = "${var.product}-${var.component}-${var.env}"
+  resource_group_name = "${var.product}-${var.component}-${var.env}"
+}
+
+resource "azurerm_key_vault_access_policy" "jenkins" {
+  key_vault_id = data.azurerm_key_vault.case_loader.id
+  tenant_id    = var.tenant_id
+  object_id    = data.azurerm_user_assigned_identity.jenkins.principal_id
+
+  certificate_permissions = [
+    "Create",
+    "Delete",
+    "DeleteIssuers",
+    "Get",
+    "GetIssuers",
+    "Import",
+    "List",
+    "ListIssuers",
+    "SetIssuers",
+    "Update",
+    "ManageContacts",
+    "ManageIssuers",
+  ]
+
+  key_permissions = [
+    "Create",
+    "List",
+    "Get",
+    "Delete",
+    "Update",
+    "Import",
+    "Backup",
+    "Restore",
+    "Decrypt",
+    "Encrypt",
+    "UnwrapKey",
+    "WrapKey",
+    "Sign",
+    "Verify",
+    "GetRotationPolicy",
+  ]
+
+  secret_permissions = [
+    "Set",
+    "List",
+    "Get",
+    "Delete",
+    "Recover",
+    "Purge",
+  ]
+}
